d44d7b0e8c715e4eea6dab6cfb55ffad3c206641
あまみや ゆうこ <1@1234.sh>
2016-05-11 13:21:39 +0800

    Implement left side bar ui

--- Polemo.NetCore.Client/www/controllers/homeController.js
+++ Polemo.NetCore.Client/www/controllers/homeController.js
@@ -1,5 +1,20 @@
 ﻿router.get('/home/index', function (req, res, next) {
+    // Ace editor
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/csharp");
+
+    // Side bar tab click events
+    $('#tabWorking').click(function () {
+        $('#tabWorking').addClass('active');
+        $('#tabDirectory').removeClass('active');
+        $('.sidebar-directory').hide();
+        $('.sidebar-working').show();
+    });
+    $('#tabDirectory').click(function () {
+        $('#tabDirectory').addClass('active');
+        $('#tabWorking').removeClass('active');
+        $('.sidebar-directory').show();
+        $('.sidebar-working').hide();
+    });
 });
