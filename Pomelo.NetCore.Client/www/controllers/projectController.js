77d7b90c0faaf9996e541fed9fdc56a28de32f6c
あまみや ゆうこ <1@1234.sh>
2016-05-20 07:34:07 +0800

    Implement project list

--- Pomelo.NetCore.Client/www/controllers/projectController.js
+++ Pomelo.NetCore.Client/www/controllers/projectController.js
@@ -1,13 +1,2 @@
-﻿function resizeOpenList(res)
-{
-    var count = parseInt($(window).width() / 202);
-    res.find('.open-project-outer').outerWidth(count * 202);
-}
-
-router.get('/project/index', function (req, res, next) {
-    // Responsive Design
-    $(window).resize(function () {
-        resizeOpenList(res);
-    });
-    resizeOpenList(res);
+﻿router.get('/project/index', function (req, res, next) {
 });
