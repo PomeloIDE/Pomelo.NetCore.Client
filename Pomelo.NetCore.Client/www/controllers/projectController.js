83f817400d0cce800321aa3f902201fecac886a6
あまみや ゆうこ <1@1234.sh>
2016-05-25 09:37:20 +0800

    Butt to get projects api

--- Pomelo.NetCore.Client/www/controllers/projectController.js
+++ Pomelo.NetCore.Client/www/controllers/projectController.js
@@ -1,20 +1,32 @@
 ﻿router.get('/project/index', function (req, res, next) {
-    $('.project .open-project-item .open').click(function () {
-        showMsg("Loading...");
-        var proj = $(this);
-        node.invoke('OpenProject', proj.attr('data-project'), proj.attr('data-git'), proj.attr('data-usr'), proj.attr('data-pwd'), proj.attr('data-email'))
-            .done(function (data) {
-                if (data.isSucceeded) {
-                    showMsg("Starting OmniSharp Host...");
-                    node.invoke('StartOmnisharp', proj.attr('data-project'))
-                        .done(function () {
-                            jFlick.RedirectTo('~/work/index.html?project=' + proj.attr('data-project'));
-                        });
-                } else {
-                    showMsg('An error occurred during loading the project. <br/> ' + data.msg, 3000);
+    showMsg('Loading project list...');
+    server.invoke('GetProjects')
+        .done(function (data) {
+            if (data) {
+                for (var i = 0; i < data.length; i++) {
+                    $('.open-project-outer').append('<div class="open-project-item"><div class="icon"><i class="fa fa-cloud"></i></div><div class="name">' + data[i].Project + '</div><div class="buttons"><a href="javascript:;" class="btn open" data-project="' + data[i].Project + '" data-git="' + data[i].Git + '" data-pwd="' + data[i].Password + '" data-usr="' + data[i].Name + '" data-email="' + data[i].Email + '"><i class="fa fa-folder-open"></i></a><a href="javascript:;" class="btn remove"><i class="fa fa-trash"></i></a></div></div>');
                 }
-            });
-    });
+                $('.project .open-project-item .open').click(function () {
+                    showMsg("Loading project...");
+                    var proj = $(this);
+                    node.invoke('OpenProject', proj.attr('data-project'), proj.attr('data-git'), proj.attr('data-usr'), proj.attr('data-pwd'), proj.attr('data-email'))
+                        .done(function (data) {
+                            if (data.isSucceeded) {
+                                showMsg("Starting OmniSharp Host...");
+                                node.invoke('StartOmnisharp', proj.attr('data-project'))
+                                    .done(function () {
+                                        jFlick.RedirectTo('~/work/index.html?project=' + proj.attr('data-project'));
+                                    });
+                            } else {
+                                showMsg('An error occurred during loading the project. <br/> ' + data.msg, 3000);
+                            }
+                        });
+                });
+                hideMsg();
+            } else {
+                showMsg('No project found.', 3000);
+            }
+        });
 
     $('.project .header-center-item.create-project').click(function () {
         $('.project .body').addClass('hidden');
